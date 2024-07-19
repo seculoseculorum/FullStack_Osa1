bc85889f8e6dc774b2436e4a875d292a68ac115c
Jussi Laitinen <jussi@seculorum.fi>
2024-07-19 14:37:53 +0300

    Changes App.jsx

--- Osa1_1/src/App.jsx
+++ Osa1_1/src/App.jsx
@@ -1,25 +1,28 @@
-const Hello = (props) => {
-  console.log(props)
-  return (
+const App =  () =>{
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of react'¨
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return(
     <div>
+      <h1>{course}</h1>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        {part1} {exercises1}
       </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+
+
     </div>
   )
+
 }
-
-const App = () => {
-  const nimi = 'Pekka'
-  const ika = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
-    </div>
-  )
-}
-
-export default App
